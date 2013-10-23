41fdb39b79d990b4397c005b38ab3b077acbacc6
rixardxxd <rixardxxd@gmail.com>
2013-10-22 17:40:31 -0700

    update

--- food2cube/static/mainSite/app/scripts/app.js
+++ food2cube/static/mainSite/app/scripts/app.js
@@ -4,11 +4,11 @@ angular.module('food2cubeApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'http://localhost:5000/static/mainSite/app/views/main.html',
         controller: 'MainCtrl'
       })
       .when('/company/:companyName', {
-        templateUrl: 'views/company.html',
+        templateUrl: 'http://localhost:5000/static/mainSite/app/views/company.html',
         controller: 'CompanyCtrl'
       })
       .otherwise({
--- food2cube/static/mainSite/app/scripts/services/Company.js
+++ food2cube/static/mainSite/app/scripts/services/Company.js
@@ -5,8 +5,8 @@ angular.module('food2cubeApp')
     var items = {};
     items.query = function() {
       return [
-        {id: 1, name: 'Oracle', img: '/img/oracle.jpg'},
-        {id: 2, name: 'Yahoo', img: '/img/yahoo.png'}
+        {id: 1, name: 'Oracle', img: 'http://localhost:5000/static/mainSite/app/img/oracle.jpg'},
+        {id: 2, name: 'Yahoo', img: 'http://localhost:5000/static/mainSite/app/img/yahoo.png'}
       ];
     };
     return items;
