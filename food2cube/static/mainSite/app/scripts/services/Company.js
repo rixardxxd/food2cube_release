7b729cc7553f9820777936a87b8bf42d8041a8f0
rixardxxd <rixardxxd@gmail.com>
2013-10-27 22:10:28 -0700

    heroku

--- food2cube/static/mainSite/app/scripts/services/Company.js
+++ food2cube/static/mainSite/app/scripts/services/Company.js
@@ -5,8 +5,8 @@ angular.module('food2cubeApp')
     var items = {};
     items.query = function() {
       return [
-        {id: 1, name: 'Oracle', img: 'http://localhost:5000/static/mainSite/app/img/oracle.jpg'},
-        {id: 2, name: 'Yahoo', img: 'http://localhost:5000/static/mainSite/app/img/yahoo.png'}
+        {id: 1, name: 'Oracle', img: 'static/mainSite/app/img/oracle.jpg'},
+        {id: 2, name: 'Yahoo', img: 'static/mainSite/app/img/yahoo.png'}
       ];
     };
     return items;
