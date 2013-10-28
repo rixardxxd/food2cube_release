559229b8751e8c204fc1502a222451773dee9150
rixardxxd <rixardxxd@gmail.com>
2013-10-27 20:19:55 -0700

    static files

--- food2cube/static/mainSite/app/scripts/controllers/Login.js
+++ food2cube/static/mainSite/app/scripts/controllers/Login.js
@@ -1,7 +1,39 @@
 'use strict';
 
+//angular.module('food2cubeApp')
+//  .controller('LoginCtrl', ['$scope', 'UserProfile', function ($scope, UserProfile) {
+//    $scope.isLoggedIn = UserProfile.isLoggedIn();
+//    $scope.doLogin = function() {
+//      UserProfile.doLogin($scope.user,function(loginFailed,user){
+//          $scope.loginFailed = loginFailed;
+//          $scope.user = user;
+//          if(!loginFailed)
+//          $('#signin').modal('hide')
+//      });
+//    }
+//  }]);
+
+
 angular.module('food2cubeApp')
   .controller('LoginCtrl', ['$scope', 'UserProfile', function ($scope, UserProfile) {
+    $scope.isLoggedIn = UserProfile.isLoggedIn();
+    $scope.doLogin = function() {
+      UserProfile.doLogin($scope.user).then(
+          function(data){
+            $scope.loginFailed = false;
+            $('#signin').modal('hide');
+          },
+          function(data){
+             $scope.loginFailed = true;
+          }
+      );
+
+    }
+  }]);
+
+
+angular.module('food2cubeApp')
+  .controller('SignupCtrl', ['$scope', 'UserProfile', function ($scope, UserProfile) {
     $scope.isLoggedIn = UserProfile.isLoggedIn();
     $scope.doLogin = function() {
       UserProfile.doLogin($scope.user);
--- food2cube/static/mainSite/app/scripts/services/UserProfile.js
+++ food2cube/static/mainSite/app/scripts/services/UserProfile.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('food2cubeApp')
-  .factory('UserProfile', ['$http', function ($http) {
+  .factory('UserProfile', ['$http','$q', function ($http,$q) {
     var loggedIn = false;
+    var loggedInFailed = false;
+    var signedup = false;
     var firstName;
     var lastName;
 
@@ -11,17 +13,48 @@ angular.module('food2cubeApp')
         return loggedIn;
       },
       doLogin: function (data) {
+
+        var deferred = $q.defer();
         $http({
           method : 'POST',
-          url : '/api/login',
+          url : '/api/login/',
           data : data
         }).success(function (data, status, headers, config) {
             loggedIn = true;
-            firstName = data.firstName;
-            lastName = data.lastName;
+            loggedInFailed = false;
+            deferred.resolve(data);
+
+
         }).error(function (data, status, headers, config) {
             //TODO: Show error message
+            loggedIn = false;
+            loggedInFailed = true;
+            deferred.reject(data);
+
+
+
         });
-      }
+
+          return deferred.promise;
+      },
+
+        doSignup: function(data,callback){
+            $http({
+                method : 'POST',
+                url : '/api/signup',
+                data: data
+            }).success(function (data,status,headers,config){
+                    signedup = true;
+                    callback(signedup);
+            }).error(function (data, status, headers, config) {
+            //TODO: Show error message
+                    signedup = false;
+                    callback(signedup);
+
+
+            });
+
+        }
+
     };
   }]);
