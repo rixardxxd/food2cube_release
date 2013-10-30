dc0a9e4e843710603c3f1c42e797019837411c1b
amity177 <amity.mio@gmail.com>
2013-10-29 23:01:40 -0700

    resovle conflict

--- food2cube/static/mainSite/app/scripts/app.js
+++ food2cube/static/mainSite/app/scripts/app.js
@@ -4,19 +4,12 @@ angular.module('food2cubeApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-<<<<<<< HEAD
-        templateUrl: 'http://localhost:5000/static/mainSite/app/views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/company/:companyName', {
-        templateUrl: 'http://localhost:5000/static/mainSite/app/views/company.html',
-=======
+
         templateUrl: 'static/mainSite/app/views/main.html',
         controller: 'MainCtrl'
       })
       .when('/company/:companyName', {
         templateUrl: 'static/mainSite/app/views/company.html',
->>>>>>> d8468662c29cb3dc85ec6de185a37c744d07368c
         controller: 'CompanyCtrl'
       })
       .otherwise({
