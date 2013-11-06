22d1b98f1fbed3c5830952a6bcf16ea815c93d1f
fangyue <fangyuew@yahoo-inc.com>
2013-11-06 00:34:57 -0800

    checkout page updated to support item edit, sort and proceed checkout

--- food2cube/static/mainSite/app/scripts/controllers/checkout.js
+++ food2cube/static/mainSite/app/scripts/controllers/checkout.js
@@ -1,13 +1,22 @@
 'use strict';
 
 angular.module('food2cubeApp')
-  .controller('CheckoutCtrl', ['$scope', 'Checkout',
-        function ($scope, Checkout)
+  .controller('CheckoutCtrl', ['$scope', 'Checkout', '$http',
+        function ($scope, Checkout, $http)
         {
-            $scope.items = Checkout.query();
+            //In practise, $scope.items should be added in former page, not generated in service.
+            $scope.items = Checkout.queryTestData();
 
             console.log("items = ");
             console.log($scope.items);
+
+            $scope.doUpdateAmount = function(id, amount)
+            {
+                console.log("Update Item Amount : id="+id+" New Amount="+amount);
+                Checkout.updateAmount($scope.items, id, amount);
+                console.log("items = ");
+                console.log($scope.items);
+            }
 
         }
     ]);
