b25d5f98dec1809cd5363ff21c8690f19b37938d
rixardxxd <rixardxxd@gmail.com>
2014-02-06 00:15:26 -0800

    test2

--- mainSite/ipnAction.py
+++ mainSite/ipnAction.py
@@ -3,6 +3,7 @@ import logging
 def save_payment_and_send_email(sender, **kwargs):
     ipn_obj = sender
     log = logging.getLogger(__name__)
+    log.info('aaaaaaaaa')
     log.info(sender)
 
-payment_was_successful.connect(save_payment_and_send_email())
+payment_was_successful.connect(save_payment_and_send_email)
