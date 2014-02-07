02a358df67796f3dd8ff45dab5d08ca9de408968
rixardxxd <rixardxxd@gmail.com>
2014-02-07 15:07:56 -0800

    fix the logger issue

--- mainSite/ipnAction.py
+++ mainSite/ipnAction.py
@@ -2,7 +2,7 @@ from paypal.standard.ipn.signals import payment_was_successful
 import logging
 def save_payment_and_send_email(sender, **kwargs):
     ipn_obj = sender
-    log = logging.getLogger(__name__)
+    log = logging.getLogger('mainSite')
     log.info('aaaaaaaaa')
     log.info(sender)
     print __file__,1, 'This works'
