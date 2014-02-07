5168d5a5e649fa0e5f792c2cd22ec493d8b56df9
rixardxxd <rixardxxd@gmail.com>
2014-02-07 14:35:43 -0800

    fix url

--- food2cube/static/mainSite/app/scripts/controllers/checkout.js
+++ food2cube/static/mainSite/app/scripts/controllers/checkout.js
@@ -11,7 +11,7 @@
                     return: "http://morning-cove-6125.herokuapp.com/#/ordercompletion",
                     no_shipping: "1",
                     cn: "Will be delivered to address 1",
-                    notify_url: "http://morning-cove-6125.herokuapp.com/paypalipnlistener/"
+                    notify_url: "http://morning-cove-6125.herokuapp.com/paypalipnlistener"
 
                 };
 var addresses = [
