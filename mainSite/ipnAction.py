2a8e2a163418d4b040284bcd1aec667307531b55
rixardxxd <rixardxxd@gmail.com>
2014-02-11 23:19:50 -0800

    test

--- mainSite/ipnAction.py
+++ mainSite/ipnAction.py
@@ -1,9 +1,10 @@
-from paypal.standard.ipn.signals import payment_was_successful
+from paypal.standard.ipn.signals import payment_was_successful,payment_was_flagged
 import logging
+log = logging.getLogger(__name__)
+
+
 def save_payment_and_send_email(sender, **kwargs):
     ipn_obj = sender
-
-    log = logging.getLogger('mainSite')
     log.info('aaaaaaaaa')
     log.info(sender)
     print __file__,1, 'This works'
@@ -12,5 +13,9 @@ def save_payment_and_send_email(sender, **kwargs):
 
 
 
+def payment_flagged(sender, **kwargs):
+    print "FLAGGED: %s" % sender.payer_email
+
 #Here is the payment successful signal passed from the django paypal
 payment_was_successful.connect(save_payment_and_send_email)
+payment_was_flagged.connect(payment_flagged)
