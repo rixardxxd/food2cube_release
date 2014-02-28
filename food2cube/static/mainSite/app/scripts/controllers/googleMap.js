c9d5e7b261852f9d176b98f3c02087a00d688986
xxd <xxd@yahoo-inc.com>
2014-02-28 14:44:04 -0800

    maps

--- food2cube/static/mainSite/app/scripts/controllers/googleMap.js
+++ food2cube/static/mainSite/app/scripts/controllers/googleMap.js
@@ -4,8 +4,8 @@ angular.module('food2cubeApp')
   .controller('googleMapCtrl', ['$scope', function ($scope) {
         $scope.map = {
             center: {
-                latitude: -121.9191404,
-                longitude: 37.4137296
+                latitude: 37.418591,
+                longitude: -121.919399
             },
             zoom: 4
         };
